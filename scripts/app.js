aaff040c5caa9eafb98fab825f0b7cc2949c355f
Shane-sama <shane.roellin@icloud.com>
2023-06-16 10:06:41 +0200

    Neko improved
    Script now creates HTML-element instead of using already existing one.

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,17 @@
 function callNeko(){
+    var elementExists = document.getElementById("neko");
+    if(!elementExists){
+        const img = new Image(300);
+        img.id = "neko";
+        document.body.appendChild(img);
+        document.getElementById("mainDiv");
+    }
+
     fetch('https://nekos.best/api/v2/neko')
         .then(response => response.json())
         .then(json => {
         console.log(json.results[0].url)
         img_url = json.results[0].url
-        document.getElementById("neko").src = img_url
-        })
+        document.getElementById("neko").src = img_url;
+    })
 }
