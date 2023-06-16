f578e7af5244dea4fed90a2691390e0803cf0afd
Shane-sama <shane.roellin@icloud.com>
2023-06-16 08:54:41 +0200

    added callNeko
    callNeko function adds neko image to website, which is called from NekosBest-api:
    https://nekos.best/
    Similar to old Project:
    https://github.com/Shane-sama/Neko-Tinder

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+function callNeko(){
+    fetch('https://nekos.best/api/v2/neko')
+        .then(response => response.json())
+        .then(json => {
+        console.log(json.results[0].url)
+        img_url = json.results[0].url
+        document.getElementById("neko").src = img_url
+        })
+}
